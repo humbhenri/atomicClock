80c9761c29e623a634b779abe1d673251a022d2b
humberto <humbhenri@gmail.com>
2014-05-20 21:06:52 -0300

    fix

--- atomicClock.go
+++ atomicClock.go
@@ -12,12 +12,13 @@ func main() {
 	resp, err := http.Get("http://en.uhrzeit.org/atomic-clock.php")
 	if err != nil {
 		fmt.Println("Cannot get time from internet.")
+	} else {
+		defer resp.Body.Close()
+
+		regex := regexp.MustCompile("(?:<div id=\"anzeige_zeit\">(.*)</div>)")
+
+		body, _ := ioutil.ReadAll(resp.Body)
+		matches := regex.FindSubmatch(body)
+		fmt.Println(string(matches[1]))
 	}
-	defer resp.Body.Close()
-
-	regex := regexp.MustCompile("(?:<div id=\"anzeige_zeit\">(.*)</div>)")
-
-	body, err := ioutil.ReadAll(resp.Body)
-	matches := regex.FindSubmatch(body)
-	fmt.Println(string(matches[1]))
 }
